46fa65c1a8ed1e42919d1ba484a0141ae8b8780f
Nathaniel <nathaniel.rink@celect.com>
2018-06-12 18:55:29 -0400

    Rid ourselves of the static components and their registrations

--- dynamic-async-components-demo/src/components/Playground-async.vue
+++ dynamic-async-components-demo/src/components/Playground-async.vue
@@ -3,9 +3,6 @@
     <h1>Welcome to the component playground!</h1>
     <p>Note that this was lazy loaded ON DEMAND because it was imported in the router definition. Prove it by looking at the
       network tab when you change routes - a call for an additional .js file goes out</p>
-    <my-button/>
-    <my-header/>
-    <my-text-input/>
   </section>
 </template>
 
@@ -16,13 +13,6 @@
 
   export default {
     name: 'Playground-Async',
-
-    //Register components - Not necessary for the selectable dynamic component
-    components: {
-      'my-button': Button,
-      'my-header': Header,
-      'my-text-input': TextInput
-    },
 
     //data holds the controller for the component. Must be a return function
     data: function () {
