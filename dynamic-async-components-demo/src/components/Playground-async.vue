4312eedff29c705e9dfd4a5493aa094480e66c4e
Nathaniel <nathaniel.rink@celect.com>
2018-06-12 18:50:26 -0400

    Add Button, Header, TextInput components and display them

--- dynamic-async-components-demo/src/components/Playground-async.vue
+++ dynamic-async-components-demo/src/components/Playground-async.vue
@@ -3,11 +3,23 @@
     <h1>Welcome to the component playground!</h1>
     <p>Note that this was lazy loaded ON DEMAND because it was imported in the router definition. Prove it by looking at the
       network tab when you change routes - a call for an additional .js file goes out</p>
+    <my-button/>
+    <my-header/>
+    <my-text-input/>
   </section>
 </template>
 
 <script>
+  import Button from '@/components/dynamic/Button'
+  import Header from '@/components/dynamic/Header'
+  import TextInput from '@/components/dynamic/TextInput'
+
   export default {
-    name: 'Playground-Async'
+    name: 'Playground-Async',
+    components: {
+      'my-button': Button,
+      'my-header': Header,
+      'my-text-input': TextInput
+    }
   }
 </script>
