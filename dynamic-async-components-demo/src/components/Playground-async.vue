8a8deb93a5ec5fa32d227837da239bd921b44dab
Nathaniel <nathaniel.rink@celect.com>
2018-06-12 19:00:05 -0400

    Add dropdown of available components

--- dynamic-async-components-demo/src/components/Playground-async.vue
+++ dynamic-async-components-demo/src/components/Playground-async.vue
@@ -3,6 +3,21 @@
     <h1>Welcome to the component playground!</h1>
     <p>Note that this was lazy loaded ON DEMAND because it was imported in the router definition. Prove it by looking at the
       network tab when you change routes - a call for an additional .js file goes out</p>
+
+    <!--selectedComponent is 2-way bound to the select value-->
+    <select v-model="selectedComponent">
+
+      <!--each component in the componentList is a value in the option dropdown-->
+      <!-- including a key is best practice so in-place-patch strategy doesn't cause unexpected results-->
+      <option
+        v-for="(component, index) in componentList"
+        :key="index"
+        :value="component"
+      >
+        {{ component.name }}
+      </option>
+    </select>
+
   </section>
 </template>
 
