cbc2d479246bd3f06a3ee6221f9f3636fe1d8fd1
Nathaniel <nathaniel.rink@celect.com>
2018-06-22 17:08:06 -0400

    Add router-demo project rename animation to component-demo project add bootstrap to router-demo

---
+++ router-demo/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <nav-bar></nav-bar>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import NavBar from './components/NavBar'
+
+export default {
+  name: 'app',
+  components: {
+	  NavBar,
+    HelloWorld
+  }
+}
+</script>
+
+
+<style lang="scss">
+  @import '../node_modules/bootstrap/scss/bootstrap.scss';
+</style>
+
---
+++ router-demo/src/components/NavBar.vue
@@ -0,0 +1,12 @@
+<template>
+<div>
+  Here I am. Rock me Like a Hurricane!
+</div>
+</template>
+
+<script>
+export default {
+  name: 'NavBar'
+}
+</script>
+
