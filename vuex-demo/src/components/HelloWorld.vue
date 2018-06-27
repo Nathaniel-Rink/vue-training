78c8e6d8543d4e4457322a2f60cc235c083521d6
Nathaniel <nathaniel.rink@celect.com>
2018-06-27 13:49:30 -0400

    Use vuex.mapState for easier computed state getters

--- vuex-demo/src/components/HelloWorld.vue
+++ vuex-demo/src/components/HelloWorld.vue
@@ -1,11 +1,21 @@
 <template>
-  <div class="hello">
-    <button @click="incrementCount">increment</button>
-    <p>{{count}}</p>
-  </div>
+	<div class="hello">
+		<button @click="incrementCount">increment</button>
+		<p>{{countVerbose}}</p>
+		<p>{{countShort}}</p>
+		<p>{{countShorter}}</p>
+		<p>{{count}}</p>
+
+	</div>
 </template>
 
 <script>
+	//Import mapState where you use it
+	import { mapState } from 'vuex';
+
+	//Import lodash where you use it
+	import _ from 'lodash';
+
 	export default {
 		name: 'HelloWorld',
 		methods: {
@@ -13,9 +23,33 @@
 				this.$store.commit('increment');
 			}
 		},
-		computed: {
-			count: function(){return this.$store.state.count}
-                }
+
+
+		// computed expects an object
+		computed:
+
+			//mapState returns. but need to merge it if we have local computed calcs
+			_.merge(
+
+				{
+					// typical way to return a state variable
+					countVerbose: function(){
+						return this.$store.state.count;
+					}
+				},
+
+				mapState({
+					// same function as countVebose but in arrow function format
+					countShort: state => state.count,
+
+					// passing the string value 'count' to mapState is same as `state => state.count`
+					countShorter: 'count'
+				}),
+
+				// mapState can also be passed an array of state property names
+				mapState(['count'])
+			)
+
 	}
 </script>
 
